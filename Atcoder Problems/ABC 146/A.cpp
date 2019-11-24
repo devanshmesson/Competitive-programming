809642126754bf26df80a30d5fbcf930e702aa6e
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2019-11-24 21:00:23 +0530

    Create A.cpp

---
+++ Atcoder Problems/ABC 146/A.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n,a;
+    cin>>n;
+    string s;
+    cin>>s;
+
+    for(int i=0;i<s.size();i++)
+    {
+        a=s[i];
+        a+=n;
+        if(a>90){a=a-90;a+=64;}
+        char c=char(a);
+        cout<<c;
+    }
+}
+
+
+
+
+
+
+
+
+
