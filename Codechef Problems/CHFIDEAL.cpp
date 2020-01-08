e9933af52f02f18f918063a241e41149591b6911
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-01-08 19:08:36 +0530

    Create CHFIDEAL.cpp

---
+++ Codechef Problems/CHFIDEAL.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #define int long long int
+    int n;
+    cout<<1<<endl;
+    fflush(stdout);
+    cin>>n;
+    if(n==2)cout<<3<<endl;
+    else cout<<2<<endl;
+    fflush(stdout);
+    return 0;
+}
