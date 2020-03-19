2a38960060a93afc24539c4c1ae788a65e12cdb2
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-03-19 18:11:25 +0530

    Create A.cpp

---
+++ Atcoder Problems/Panasonic Programming Contest 2020/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+   #define int long long int
+   int a[]={1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+   int k;
+   cin>>k;
+   cout<<a[k-1]<<endl;
+
+    return 0;
+}
