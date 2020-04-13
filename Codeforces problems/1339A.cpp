7eb8af05bfe7259c7ee372ecc8ef49134979dec1
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-04-14 02:47:45 +0530

    Create 1339A.cpp

---
+++ Codeforces problems/1339A.cpp
@@ -0,0 +1,19 @@
+//A. Filling Diamonds
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #define int long long int
+    #define pb push_back
+    #define mk make_pair
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n<<endl;
+    }
+    return 0;
+}
