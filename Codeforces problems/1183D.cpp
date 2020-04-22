68ca3579954726fcddcbcd9b4059f01578b09922
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-04-23 05:25:18 +0530

    Update 1183D.cpp

--- Codeforces problems/1183D.cpp
+++ Codeforces problems/1183D.cpp
@@ -10,47 +10,42 @@ int main()
     cin>>t;
     while(t--)
     {
-       int n,a,sz=0,sum=0;
+       int n,a,sum=0,sz=0;
        cin>>n;
-       vector<int>v(n+1000);
-       vector<int>k;
-       set<int>w;
+       vector<int>v(n+1);
        for(int i=0;i<n;i++)
        {
            cin>>a;
            v[a]++;
-           w.insert(a);
-
-       }
-int h,r;
-       for(auto j=w.begin();j!=w.end();j++)
-       {
-           h=*j;
-           r=v[h];
-           k.pb(r);
            sz++;
        }
-       sort(k.begin(),k.end(),greater<int>());
-       vector<int>visit(n+1000);
-       visit[k[0]]=1;
-       for(int i=1;i<k.size();i++)
+       sort(v.begin(),v.end(),greater<int>());
+       vector<int>visit(n+1);
+       visit[v[0]]=1;
+       for(int i=1;i<sz;i++)
        {
-        if(visit[k[i]]==1)
+        if(visit[v[i]]==1)
         {
-            if(k[i-1]>0 && (k[i-1]-1)>0){k[i]=k[i-1]-1;}
-            else k[i]=0;
-            visit[k[i]]=1;
+            if(v[i-1]>0 && (v[i-1]-1)>0){v[i]=v[i-1]-1;}
+            else v[i]=0;
+            visit[v[i]]=1;
         }
-        else visit[k[i]]=1;
+        else visit[v[i]]=1;
        }
-
-       for(int i=0;i<k.size();i++)
+       for(int i=0;i<sz;i++)
        {
-         sum+=k[i];
+         sum+=v[i];
        }
        cout<<sum<<endl;
 
 
     }
+
+
+
+
+
+
+
     return 0;
 }
