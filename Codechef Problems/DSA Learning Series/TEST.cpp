b5be27203adbb7a14a7605ce763094c1986c9d5a
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-04-27 00:36:30 +0530

    Create TEST.cpp

---
+++ Codechef Problems/DSA Learning Series/TEST.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #define int long long int
+    #define pb push_back
+    #define mk make_pair
+    int t;
+    while(1)
+    {
+        cin>>t;
+        if(t!=42)cout<<t<<endl;
+        else break;
+        
+    }
+    return 0;
+}
+
+
