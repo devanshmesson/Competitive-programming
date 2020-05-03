8df8bfc3d943447cc8615fbd7c80e12b28272474
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-05-03 21:16:32 +0530

    Create 1187A.cpp

---
+++ Codeforces problems/1187A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,s,t,mi,ma;
+        cin>>n>>s>>t;
+        if(s>t)
+        {
+            mi=t;
+            ma=s;
+        }
+        else
+        {
+            mi=s; ma=t;
+        }
+
+        int ss=n-ma;
+        mi-=ss; //st
+        int tt=n-(ss+mi);
+
+        cout<<max(ss,tt)+1<<endl;
+
+
+
+
+
+
+
+    }
+
+    return 0;
+}
