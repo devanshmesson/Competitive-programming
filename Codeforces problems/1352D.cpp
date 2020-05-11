e44bc27955da7e8d31448a8098e2038dbc261d62
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-05-11 11:20:18 +0530

    Update 1352D.cpp

--- Codeforces problems/1352D.cpp
+++ Codeforces problems/1352D.cpp
@@ -9,6 +9,7 @@ int32_t main()
     while(t--)
     {
        int n,l=0,r,ansl=0,ansr=0,lastl=0,lastr=0,presl=0,presr=0,cnt=0,sumr=0,suml=0;
+       map<int,int>visit;
        cin>>n;
        int a[n+1];
        for(int i=0;i<n;i++)cin>>a[i];
@@ -20,8 +21,12 @@ int32_t main()
 
            while(l<=r && presl<=sumr)
            {
+               if(visit[l]==0)
+               {
+               visit[l]++;
                presl+=a[l];
                l++;
+               }
            }
            ansl+=presl;
            suml=presl;
@@ -32,8 +37,12 @@ int32_t main()
 
            while(l<=r && presr<=suml)
            {
+               if(visit[r]==0)
+               {
+               visit[r]++;
                presr+=a[r];
                r--;
+               }
            }
            ansr+=presr;
            sumr=presr;
