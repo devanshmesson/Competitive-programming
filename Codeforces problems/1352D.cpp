2139c3e26e005c5fe653f0d3567fde31a7f622b0
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-05-11 08:43:00 +0400

    Create 1352D.cpp

---
+++ Codeforces problems/1352D.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       int n,l=0,r,ansl=0,ansr=0,lastl=0,lastr=0,presl=0,presr=0,cnt=0,sumr=0,suml=0;
+       cin>>n;
+       int a[n+1];
+       for(int i=0;i<n;i++)cin>>a[i];
+       r=n-1;
+       while(l<=r)
+       {
+           if(cnt%2==0)
+           {
+
+           while(l<=r && presl<=sumr)
+           {
+               presl+=a[l];
+               l++;
+           }
+           ansl+=presl;
+           suml=presl;
+           presl=0;
+           }
+           else if(cnt%2!=0)
+           {
+
+           while(l<=r && presr<=suml)
+           {
+               presr+=a[r];
+               r--;
+           }
+           ansr+=presr;
+           sumr=presr;
+           presr=0;
+           }
+           cnt++;
+       }
+       cout<<cnt<<" "<<ansl<<" "<<ansr<<endl;
+
+    }
+}
