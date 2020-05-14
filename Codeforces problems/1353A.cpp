e9ac6a679e41f50cb30dbedd0825107738231291
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-05-14 22:11:45 +0530

    Create 1353A.cpp

---
+++ Codeforces problems/1353A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define mk make_pair
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a==1){cout<<0<<endl;continue;}
+        if(a==2){cout<<b<<endl;continue;}
+        cout<<b*2<<endl;
+
+    }
+}
