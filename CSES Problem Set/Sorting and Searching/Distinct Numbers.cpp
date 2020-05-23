dd9822ed0841907f23dc0782e12d5a863fb7e214
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-05-23 18:30:21 +0530

    Create Distinct Numbers.cpp

---
+++ CSES Problem Set/Sorting and Searching/Distinct Numbers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n,a,cnt=1;
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<n-1;i++) if(v[i]!=v[i+1])cnt++;
+    cout<<cnt<<endl;
+    return 0;
