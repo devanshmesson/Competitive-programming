4cdfd70ec43a04ef1398ab2019f6a43f858f652e
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-06-02 21:53:59 +0530

    Update Subarray Sums I.cpp

--- CSES Problem Set/Sorting and Searching/Subarray Sums I.cpp
+++ CSES Problem Set/Sorting and Searching/Subarray Sums I.cpp
@@ -4,28 +4,25 @@
           -Pref[High]-Pref[Low-1]=x
           -High will be all elements of array(by traversal)
           -To Calculate Low where condition satisfies => pref[low]=pref[High]-x
-          -Put all prefix sum values in a hash table(unordered map) and check if(pref[High]-x) is there in hash table or not.
+          -Put prefix sum values till the "High"th index in a hash table(unordered map) and check if(pref[High]-x) is there in hash table or not.
 include<bits/stdc++.h>
 using namespace std;
 main() 
 {
+  io
   int n,x;
   cin>>n>>x;
   int a[n+1];
   for(int i=0;i<n;i++)cin>>a[i];
-  int pref[n+1]={0};
-  unordered_map<int,int>hash;
+  map<int,int>hash;
   hash[0]++;
+  int ans=0,pref=0;
   for(int i=0;i<n;i++)
   {
-    pref[i]=a[i]+(i==0?0:pref[i-1]);
-    hash[pref[i]]++;
-  }
-  int ans=0;
-  for(int i=0;i<n;i++)
-  {
-    int need=pref[i]-x;
+    pref+=a[i];
+    int need=pref-x;
     ans+=hash[need];
+    hash[pref]++;
   }
   cout<<ans<<endl;
 }
