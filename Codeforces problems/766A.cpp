637c89e6327371b5d83c02b69085c07b022dd1ee
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-06-13 18:05:46 +0530

    Create 766A.cpp

---
+++ Codeforces problems/766A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ main() 
+{
+  string s,t;
+  cin>>s>>t;
+  if(s==t){cout<<-1<<endl;return 0;}
+  int sz=s.size(),tz=t.size(),ans=0;
+  cout<<max(sz,tz)<<endl;
+  return 0;
+}
+ 
+  
+ 
+ 
