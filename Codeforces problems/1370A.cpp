2f61c2fb8eb04ed27ca9c81973753776930f84e5
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-06-20 22:32:01 +0530

    Create 1370A.cpp

---
+++ Codeforces problems/1370A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n,m=INT_MIN;
+    cin>>n;
+    cout<<n/2<<endl;
+  }
+ }
