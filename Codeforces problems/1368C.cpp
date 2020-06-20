f080884004d890005826f2ac040c312ab46bef5a
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-06-20 17:54:41 +0530

    Update 1368C.cpp

--- Codeforces problems/1368C.cpp
+++ Codeforces problems/1368C.cpp
@@ -1,23 +1,20 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 main()
 {
   int n;
   cin>>n;
-   
-  int i=0,j=0,ans=1;
   vector<pair<int,int>>v;
+  int i=0,j=0,ans=0;
   v.pb({i,j});
-
   while(1)
   {
     v.pb({i+1,j});
     v.pb({i,j+1});
     i++;j++;
+    if(i>=2)ans++;
     v.pb({i,j});
-    ans++;
-    if(ans-1>n)break;
+    if(ans==n)break;            
   }
   cout<<v.size()<<endl;
   for(auto x:v)cout<<x.first<<" "<<x.second<<endl;
