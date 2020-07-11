936fcf1b478a6920a8a5422ccfa23a36928abebe
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-07-11 22:36:37 +0530

    Create 1372A.cpp

---
+++ Codeforces problems/1372A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+main() 
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)cout<<1<<" ";
+    cout<<endl;
+   }
+  return 0;
+}
