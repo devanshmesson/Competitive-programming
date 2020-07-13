aeda3a16325ce13c818d808fe7caf3bd0a3bbfae
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-07-13 23:35:51 +0530

    Create 476B(Without Bitset).cpp

---
+++ Codeforces problems/476B(Without Bitset).cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+main() 
+{
+   #define int long long int 
+   double ans;
+   string s,t;
+   cin>>s>>t;
+   int sz=s.size();
+   int val=0;  
+   for(int i=0;i<sz;i++)
+   {
+    if(s[i]=='+')val++;
+    else val--;
+   }
+   int ok=1,valu=0,q=0;
+   for(int i=0;i<sz;i++)
+   {
+    if(t[i]=='?'){ok=0;q++;}
+    if(t[i]=='+')valu++;
+    else if(t[i]=='-')valu--;
+   }
+   if(val==valu && ok){ans=1;cout<<setprecision(10)<<fixed<<ans<<endl;return 0;}
+   else if(val!=valu && ok){ans=0;cout<<setprecision(10)<<fixed<<ans<<endl;return 0;}
+   int fin;
+   double num=0.0,den=(1<<q);
+   for(int i=0;i<den;i++)
+   {
+     fin=valu;
+     for(int j=0;j<q;j++)
+     {
+      if(((1<<j)&(i)))fin++;
+      else fin--;
+     }
+     if(fin==val)num++;
+   }
+   ans=num/den;
+   cout<<setprecision(10)<<fixed<<ans<<endl;
+  return 0;
+}
