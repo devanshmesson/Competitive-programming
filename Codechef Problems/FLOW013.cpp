66524fef217e78bb23e8b79bea76aadeaec62279
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-07-23 22:14:53 +0200

    Create FLOW013.cpp

---
+++ Codechef Problems/FLOW013.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+   int a,b,c;   
+  cin>>a>>b>>c;
+  if((a+b+c)==180)cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+ }
+}
