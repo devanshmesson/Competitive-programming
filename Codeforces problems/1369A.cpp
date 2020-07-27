1ea552aa4e4f452c2eca5280ef8b36e034a2e84f
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-07-27 23:50:34 +0530

    Create 1369A.cpp

---
+++ Codeforces problems/1369A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+main() 
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+     int n;
+     cin>>n;
+     if(n%4==0)cout<<"YES"<<endl;
+     else cout<<"NO"<<endl;
+   }
+} 
