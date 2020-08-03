ceee2488168c3e3c5c1c4d637a448ac9e7ee360c
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-08-03 19:26:58 +0530

    Create JAM11.cpp

---
+++ Dynamic Programming/Codechef/JAM11.cpp
@@ -0,0 +1,40 @@
+/*
+Problem-There are n stairs,Calculate No. of ways to reach nth stair, given that we can climb 1,2 or 3 stairs in one move.
+Time Complexity-O(T*N)
+Approach-What will be the previous step before we reach nth stair?
+        -On Previous Step, we would either be on n-1th stair or n-2th stair or n-3th stair.Because we can only climb 1,2 or 3 stairs in one move.
+        -How many ways are there to reach nth stair if we are on n-1th stair or n-2th stair or n-3th stair? Answer = 1 beacuse(read above points)
+        -How many ways are there to reach nth stair?
+        Answer-
+        Let-
+        A-No. of ways to reach n-1th stair
+        B-No. of ways to reach n-2th stair
+        C-No. of ways to reach n-3th stair
+        X-No. of ways to reach nth stair if we are on n-1th stair or n-2th stair or n-3th stair
+        
+        Final Answer= (A*X)+(B*X)+(C*X)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+  freopen
+  io
+  int t,mod=1000000007;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int dp[100001];
+    dp[0]=1;
+    dp[1]=1;
+    dp[2]=2;
+
+    for(int i=3;i<=n;i++)
+    {
+      dp[i]=(dp[i-1]+dp[i-2]+dp[i-3])%mod;
+    }
+    cout<<dp[n]<<endl;
+  }
+}
