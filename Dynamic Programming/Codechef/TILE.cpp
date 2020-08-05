52867cbc2eb57018723524d0956bc89985918f0d
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-08-05 17:49:43 +0530

    Update TILE.cpp

--- Dynamic Programming/Codechef/TILE.cpp
+++ Dynamic Programming/Codechef/TILE.cpp
@@ -1,3 +1,23 @@
+/*
+Problem-Gven a Nx2 grid,In how many ways we can fill that grid if we have 1x2 and 2x1 rectangles to fill with.
+Time Coplexity-O(T*Log(n))
+Observation-Last tile left to be filled to completely fill the whole Nx2 grid would be either 1x2 or 2x1.
+           -If the last tile is 1x2 ,then obviously the two blocks in other row in the same two columns need to be filled with 1x2.
+           They cannot be filled with 2x1.(visualize it).Now we are left with filling remaining 2x(n-2) grid 
+           -Now 2^2 grid is filled with two 1x2 tiles.
+           Let-
+           A-No. of ways to fill 2x(n-2)
+           B-No. of ways to fill 2x(2) which is 1
+           Answer for now =A*B
+           
+           -If the last tile is 2X1,then 2x1 part of the 2xn grid is filled,now we are left with filling remaining 2x(n-1) grid.
+           Let-
+           C-No. of ways to fill 2x(n-1)
+           D-No. of ways to fill 2x(1) which is 1
+           Final Answer=A*B + C*D
+           Where B and D are 1.
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
