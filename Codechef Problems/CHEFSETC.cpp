47cc628b4e68227b78f3c9b072f10d8996ce7306
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-08-05 22:55:12 +0300

    Update CHEFSETC.cpp

--- Codechef Problems/CHEFSETC.cpp
+++ Codechef Problems/CHEFSETC.cpp
@@ -2,6 +2,8 @@
 Problem-Given an array of 4 numbers,if sum of any subset of the array is 0 print "yes" else "No"
 Time Complexity-O(n*(2^n))n=4
 Approach-Generate all possible subsets by bitmasking.
+        -Just run a loop(i) till (2^4)-1 and a nested loop(j) till 4.Check if (2^j)&i==1 it means that particular bit is set.
+        -Each sub-set can be represented by a 4-bit number.
         -And sum of  those elements who have corressponding index as set in the binary representation.
 */
 #include<bits/stdc++.h>
