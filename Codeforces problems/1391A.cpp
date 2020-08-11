0ea7343526b8fbcb946bf2b90565d9fadc980cbf
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-08-10 21:10:13 -1200

    Create 1391A.cpp

---
+++ Codeforces problems/1391A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)cout<<i<<" ";
+    cout<<endl;
+  }
+}
