dde1e18e4bff63668aad21bb3596f8d572294534
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-10-14 11:37:42 +0530

    Update Bottom_up.cpp

--- Dynamic Programming/Other Problems/Knapsack and it's Variants/Minimum_Subset_Difference/Bottom_up.cpp
+++ Dynamic Programming/Other Problems/Knapsack and it's Variants/Minimum_Subset_Difference/Bottom_up.cpp
@@ -8,11 +8,10 @@ Find minimum sum difference between sum of any two subsets.
 using namespace std;
 int main()
 {
-  int n,sum=0,target;
+   int n,sum=0,target;
   cin>>n;
   int weight[n+1];
   for(int i=1;i<=n;i++){cin>>weight[i];sum+=weight[i];}
-  cout<<sum<<endl;
   target=sum/2;
   int subsetsum1,subsetsum2;
 
@@ -26,23 +25,19 @@ int main()
     check from sum/2 to 0, if in between any sum got found in tha array we will take that sum becuase difference cant be more minimum.
   */
 
-  for(int c=target;c>=0;c--)
-  {
-     int **dp=new int*[c+1];
 
-     for(int row=0;row<=c;row++)
-     {
-      dp[row]=new int[n+1];
-     }
+     int **dp=new int*[target+1];
+     for(int row=0;row<=target;row++)dp[row]=new int[n+1];
+     
       //Base case in recursion =Initialization in Bottom up dp
 
      //base case 1:target=0 then return 1
      for(int i=0;i<=n;i++)dp[0][i]=1;
 
       //base case 2: if target!=0 and n==0 then return 0
-      for(int i=1;i<=c;i++)dp[i][0]=0;
+      for(int i=1;i<=target;i++)dp[i][0]=0;
       
-      for(int i=1;i<=c;i++)
+      for(int i=1;i<=target;i++)
       {
         for(int j=1;j<=n;j++)
         {
@@ -54,10 +49,13 @@ int main()
           {
             dp[i][j]=dp[i][j-1];
           }
+           if(dp[i][n]==1)subsetsum1=i;
+           //Last row of the matrix will give us info about all sums between 1 and target such that 
+           //if there is a subset present in array whose sum = that particular column(sum).
+           //we will take the maximum sum which has dp[sum][n]=1 to minimize the difference.
         }
       }
-      if(dp[c][n]==1){subsetsum1=c;break;}
-  }
+  
   subsetsum2=sum-subsetsum1;
   int difference=abs(subsetsum1-subsetsum2);
   cout<<"Minimum difference of any two subsets in given array:"<<difference<<endl;
