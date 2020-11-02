997d6adf8a8f1609d81a533f9f2e5389cf6b4097
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-11-02 09:30:31 +0530

    Create ANDOR.cpp

---
+++ Codechef Problems/ANDOR.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  #define int long long int
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int x;
+    cin>>x;
+    cout<<0<<" "<<x<<endl;
+  } 
+}
