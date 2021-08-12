61e31760e93db58b09b48719b541218b4452f88a
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2021-08-13 03:09:35 +0530

    Update Search a 2D Matrix.cpp

--- Striver's SDE sheet/Day:3 (Array and Maths)/Search a 2D Matrix.cpp
+++ Striver's SDE sheet/Day:3 (Array and Maths)/Search a 2D Matrix.cpp
@@ -1,3 +1,23 @@
+/*
+Problem Link - https://leetcode.com/problems/search-a-2d-matrix/
+Solution:1 Traverse the whole matrix and find the element.
+Time complexity - O(n*m)
+
+Solution:2 Traverse each row or column and binary search on target element.
+Time complexity - O(NLogM) OR O(MLogN)
+
+Solution:3 
+
+Observation:
+1. 1st column is in increasing order.
+2.Figure out the ith row in which the element could be present.
+3. 1st element of ith row must be smaller or equal to target element.
+4. Binary search on the 1st column and find the greatest ith element which is smaller than or equal to target element."i" is the index of the row.
+5. Binary searc on the ith row and find the target element.
+
+Time complexity - O(LogN + LogM)
+
+*/
 class Solution {
 public:
     
