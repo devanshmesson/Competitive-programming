147febe8fedc73bba170744335bed5e63ce8c48d
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2021-08-13 23:42:53 +0530

    Update Search a 2D Matrix (GFG version).cpp

--- Striver's SDE sheet/Day:3 (Array and Maths)/Search a 2D Matrix (GFG version).cpp
+++ Striver's SDE sheet/Day:3 (Array and Maths)/Search a 2D Matrix (GFG version).cpp
@@ -8,8 +8,31 @@ Solution:2 Traverse each row or column and binary search on target element.
 Time complexity - O(NLogM) OR O(MLogN)
 
 Solution:3 
-1.Idea is to reduce the time complexity by removing a row or column in each comparison until an element is found.
+1.Idea is to reduce the time complexity by removing a row or column in each comparison until an element(X) is found.
 2.Start searching the element from top-right corner of the matrix.
+3.If X > current element, this means the element is not present in the entire row because all elements are smaller than X, so move to next row.
+4.If X < current element, this means the element is not present in the entire column because all elements are greater than X, so move to previous column.
+5.If X == current element, this means, X is found, so stop here!
+
+Why are we starting our search from top-right corner?
+-Because by starting from top right corner, we are able to remove the row/column where the element cannot exist due the condition given, which eventually reduces the size of matrix and 
+element is searched in the remaining part of tha matrix.
+
+Relate this approach to binary search - (Refer this  - https://www.youtube.com/watch?v=VS0BcOiKaGI)
+by starting from top-right corner, we have two choices to go:
+1.Down (same column,next row)
+2.Left (Same row,previous column)
+
+By Going downwards, elements >= current element are present and by Going left, elements <= current element are present.
+So, just like binary search we compare the current element with X : 
+if X > current element, go downwards and remove the row where the current element exists.
+if X < current element, go left and remove the column where the current element exists.
+if X == current element stop.
+
+
+What will happen if we start our search from top-left corner?
+-We will not be able to remove the row/column where the element cannot exist due the condition given, So we have to traverse all rows and columns to find X.
+
 
 */
 class Solution{
