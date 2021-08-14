30fae3f1c1588117d6a801ad96ef2c2f549e50df
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2021-08-14 17:47:52 +0530

    Create Majority Element.cpp

---
+++ Striver's SDE sheet/Day:3 (Array and Maths)/Majority Element.cpp
@@ -0,0 +1,52 @@
+/*
+ Problem link - https://leetcode.com/problems/majority-element/
+ Solution:1
+ Count each number's occurence by running one loop to have a pointer at a specific index and another nested loop to count the number at 
+ specific index in the whole array.(Bruteforce)
+ Time complexity - O(n^2)
+ Space complexity - O(1)
+ 
+ Solution:2
+ Sort the array and count the same adjacent elements.
+ Time complexity - O(nlOGn)
+ Space complexity - O(1)
+ 
+ Solution:3
+ Run a O(n) loop and increase the count of particular number by storing it as index of the array.In other words, peform hashing.
+ Time complexity(Map)- O(nlOGn) | Time complexity(array)- O(n)
+ Space complexity - O(1)
+ 
+ Solution:4
+ 1.Perform Moore's Voting Algorithm
+ 2.Intuition behind this algorithm is that cancel all the minority elements(let their count be x) with "x" amount of majority elements and 
+ remaining number of elements will only constitute of majority element.
+ 
+ Proof of 2: 
+ Total number of elements - n
+ Number of majority element - a
+ Number of Minority elements - b
+ a+b=n
+cancel all the minority elements(let their count be a) with "a" amount of majority elements.
+a=a-b
+ 
+ 
+  
+ 
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) 
+    {
+        //Moore's algorithm
+        int element,count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count==0)element=nums[i];
+            if(element==nums[i])count++;
+            else count--;
+        }
+        return element;
+        
+        
+    }
+};
