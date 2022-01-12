b88a5861978a0a714b528401548e4ef12bbd7464
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-01-12 23:53:56 +0530

    Create GRIDXOR.cpp

---
+++ Codechef Problems/GRIDXOR.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+signed main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<n;j++)
+      {
+        cout<<"1"<<" ";
+      }
+      cout<<endl;
+    }
+  }   
+}
+
+/*1.Never overkill a problem.
+ 2.think of test cases to clarify the approach/doubts.
+*/ 
