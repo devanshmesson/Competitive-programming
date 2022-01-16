cb465024616862a58e83958c380e2837c4933534
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-01-17 04:38:25 +0530

    Update 1626B.cpp

--- Codeforces problems/1626B.cpp
+++ Codeforces problems/1626B.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long int
+#define endl "\n"
 signed main()
 {
   int t;
@@ -12,80 +12,24 @@ signed main()
     int n=s.size();
     if(n==1){cout<<1<<endl;continue;}
     int flag=1;
-    for(int i=0;i<n-1;i++)
+    for(int i=n-1;i>=1;i--)
     {
-      int a=(int)s[i]-48;
-      int b=(int)s[i+1]-48;
-      if(a+b>=10){flag=0;break;}
+      int a=(int)s[i]-48,b=(int)s[i-1]-48;
+      if(a+b>=10)
+      {
+        s[i-1]=to_string((a+b))[0];
+        s[i]=to_string((a+b))[1];
+        flag=0;
+        break;
+      }
     }
     if(flag==1)
     {
-      int ok=0;
-      if(s[1]=='0')
-      {
-        int a=(int)s[0]-48;
-        int b=(int)s[1]-48;
-        s[0]=to_string((a+b))[0];
-        s[1]='a';  
-        ok=1;
-      }
-      if(ok==0)
-      {
-       for(int i=0;i<n-1;i++)
-       {
-         int a=(int)s[i]-48;
-         int b=(int)s[i+1]-48;
-         if(a>0 && b>0)
-         {
-           s[i]=to_string((a+b))[0];
-           s[i+1]='a';
-           ok=1;
-           break;
-         }
-       }
-     }
-     if(ok==0)
-     {
-      for(int i=0;i<n-1;i++)
-      {
-        int a=(int)s[i]-48;
-        int b=(int)s[i+1]-48;
-        if(a+b>0)
-        {
-          s[i]=to_string((a+b))[0];
-          s[i+1]='a';
-          ok=1;
-          break;
-        }
-      }
-     }
-     if(ok==0)
-     {
-      int a=(int)s[0]-48;
-      int b=(int)s[1]-48;
+      int a=(int)s[0]-48, b=(int)s[1]-48;
       s[0]=to_string((a+b))[0];
       s[1]='a';  
-     }
     }
-    else
-    {
-      for(int i=n-1;i>=1;i--)
-      {
-         int a=(int)s[i]-48;
-         int b=(int)s[i-1]-48;
-         if(a+b>=10)
-        {
-          s[i-1]=to_string((a+b))[0];
-          s[i]=to_string((a+b))[1];
-          break;
-        }
-
-      }
-    }
-    for(int i=0;i<n;i++)
-    {
-      if(s[i]!='a')cout<<s[i];
-    }
+    for(int i=0;i<n;i++)if(s[i]!='a')cout<<s[i];
     cout<<endl;
   }
 }
