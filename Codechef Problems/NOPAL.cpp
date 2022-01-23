6c5a982833744947be20640f19da12b3ec2ca632
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-01-24 04:04:37 +0530

    Create NOPAL.cpp

---
+++ Codechef Problems/NOPAL.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+
+signed main()
+{
+  freopen
+  ios
+  int t;
+  cin>>t;
+  while(t--)
+  {
+   int n;
+   cin>>n;
+   int upd=97;
+   for(int i=0;i<n;i++)
+    {
+      cout<<(char)upd;
+      upd++;
+      if(upd==123)upd=97;
+    }
+    cout<<endl;
+ }
+}
+/*1.Never overkill a problem.
+  2.think of test cases to clarify the approach/doubts.
+  3.Don't give up!
+  4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+*/ 
