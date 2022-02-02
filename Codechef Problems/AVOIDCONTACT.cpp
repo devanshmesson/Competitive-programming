e0935bee36e8f30008c411e458e7fd67d9548731
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-02 23:02:03 +0530

    Create AVOIDCONTACT.cpp

---
+++ Codechef Problems/AVOIDCONTACT.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+
+signed main()
+{
+  freopen
+  ios
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int x,y;
+    cin>>x>>y;
+    int no=x-y;
+    if(y==0){cout<<x<<endl;continue;}
+    int temp=y-1;
+    int index=0;
+    while(temp>0)
+    {
+      index+=2;
+      temp--;
+    }
+    if(no==0)
+    {
+      cout<<index+1<<endl;
+    }
+    else 
+    {
+      index+=no;
+      index+=2;
+      cout<<index<<endl;
+    } 
+  }
+}
+    /*1.Never overkill a problem.
+      2.think of test cases to clarify the approach/doubts.
+      3.Don't give up!
+      4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+      5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+    */
