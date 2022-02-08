80f280d94c0c8615a436ea0edb665e03def2c2c6
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-08 23:48:18 +0530

    Create 723A.cpp

---
+++ Striver's CP List/Implementation, Constructive/723A.cpp
@@ -0,0 +1,41 @@
+// taskkill /IM "a.exe" /F
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+
+
+
+signed main()
+{
+  freopen
+  ios
+  int a,b,c;
+  cin>>a>>b>>c;
+  cout<<max(a,max(b,c))-min(a,min(b,c))<<endl;
+
+}
+    /*1.Never overkill a problem.
+      2.think of test ca
+      ses to clarify the approach/doubts.
+      3.Don't give up!
+      4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+      5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+      6.Check datatype of variables, sometimes wrong datatype lead to WA.
+    */ 
+    
+    
+    
+    
+    
