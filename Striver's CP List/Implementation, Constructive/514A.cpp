334c22377e30e76aff0d9d424f4b7caeb3bf181b
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-08 23:46:24 +0530

    Create 514A.cpp

---
+++ Striver's CP List/Implementation, Constructive/514A.cpp
@@ -0,0 +1,47 @@
+// taskkill /IM "a.exe" /F
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+
+
+
+signed main()
+{
+  freopen
+  ios
+  string s;
+  cin>>s;
+  for(int i=0;i<s.size();i++)
+  {
+    int a=s[i]-'0';
+    int b=9-a;
+    if(i==0 && (b==0))continue;
+    s[i]=(char)min(a,b)+48;
+  }
+  cout<<s<<endl;
+}
+    /*1.Never overkill a problem.
+      2.think of test ca
+      ses to clarify the approach/doubts.
+      3.Don't give up!
+      4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+      5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+      6.Check datatype of variables, sometimes wrong datatype lead to WA.
+    */ 
+    
+    
+    
+    
+    
