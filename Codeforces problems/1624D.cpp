066c9fc367b40f95bc877bec26bd08d1cd3b6918
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-09 04:52:51 +0530

    Create 1624D.cpp

---
+++ Codeforces problems/1624D.cpp
@@ -0,0 +1,59 @@
+// taskkill /IM "a.exe" /F
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+
+
+
+signed main()
+{
+  freopen
+  ios
+  int t;
+  cin>>t;
+
+  while(t--)
+  {
+    int n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    int a[27]={0};
+    for(int i=0;i<n;i++)a[s[i]-'a'+1]++;
+    int pairs=0;
+    for(int i=1;i<=26;i++)pairs+=a[i]/2;
+    int ans=pairs/k;
+    ans*=2;
+    int left=n-(ans*k);
+    ans+=(left>=k);
+    cout<<ans<<endl;
+  }
+
+  
+
+}
+    /*1.Never overkill a problem.
+      2.think of test ca
+      ses to clarify the approach/doubts.
+      3.Don't give up!
+      4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+      5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+      6.Check datatype of variables, sometimes wrong datatype lead to WA.
+    */ 
+    
+    
+    
+    
+    
