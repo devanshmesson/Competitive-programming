70a24af21f49579b9fba978c31d429b2ed0e626c
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-11 02:26:16 +0530

    Update Increasing Subsequence.cpp

--- Dynamic Programming/CSES/Increasing Subsequence.cpp
+++ Dynamic Programming/CSES/Increasing Subsequence.cpp
@@ -14,39 +14,36 @@ using namespace std;
 #define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define int long long int
 #define float double
-vector<int>a(200009),potential(200009);
-int lis(int n)
+int lis(vector<int>&a,int n)
 {
-  for(int i=0;i<n;i++)potential[i]=1e18;
+  vector<int>potential(n,1e18);
   potential[0]=a[0];
-  int upd=0;
+  int maxi=0;
   for(int i=1;i<n;i++)
   {
-    auto x=lower_bound(potential.begin(),potential.begin()+upd+1,a[i]);
-    int lbound=(x-potential.begin());
-    if(lbound>=i)
+    int lbound=lower_bound(potential.begin(),potential.end(),a[i])-potential.begin();
+    if(lbound==0){potential[0]=a[i];}
+    else if(potential[lbound-1]<a[i]) 
     {
-      lbound--;
-    }
-    if(potential[lbound]>=a[i] && lbound>0)lbound--;
-    if(potential[lbound]>=a[i] && lbound==0)potential[lbound]=min(potential[lbound],a[i]);
-    if(potential[lbound]<a[i])
-    {
-      int lis_length=lbound+1;
-      potential[lis_length]=min(potential[lis_length],a[i]);
-      upd=max(upd,lis_length);
+      potential[lbound]=min(potential[lbound],a[i]);
+      maxi=max(maxi,lbound);
     }
   }
-  return upd+1;
+  return maxi+1;
 }
+
+
 signed main()
 {
   freopen
   ios
   int n;
   cin>>n;
+  vector<int>a(n);
   for(int i=0;i<n;i++)cin>>a[i];
-  cout<<lis(n)<<endl;
+  int lis1=lis(a,n);
+  cout<<lis1<<endl;
+  
 }
     /*1.Never overkill a problem.
       2.think of test ca
@@ -55,4 +52,9 @@ signed main()
       4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
       5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
       6.Check datatype of variables, sometimes wrong datatype lead to WA.
-    */
+    */ 
+    
+    
+    
+    
+    
