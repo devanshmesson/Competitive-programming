b4e015fa3355eb235c8736db2634db11b87c1817
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-11 02:35:32 +0530

    Update MERGEDLIS.cpp

--- Codechef Problems/MERGEDLIS.cpp
+++ Codechef Problems/MERGEDLIS.cpp
@@ -14,30 +14,23 @@ using namespace std;
 #define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define int long long int
 #define float double
-int lis(int *a,int n)
+
+int lnds(vector<int>&a,int n)
 {
-  int potential[n+1];
-  for(int i=0;i<n;i++)potential[i]=1e18;
+  vector<int>potential(n,1e18);
   potential[0]=a[0];
-  int upd=0;
+  int maxi=0;
   for(int i=1;i<n;i++)
   {
-    auto x=upper_bound(potential,potential+upd+1,a[i]);
-    int lbound=(x-potential);
-    if(lbound>=i)
+    int lbound=upper_bound(potential.begin(),potential.end(),a[i])-potential.begin();
+    if(lbound==0){potential[0]=a[i];}
+    else 
     {
-      lbound--;
-    }
-    if(potential[lbound]>a[i] && lbound>0)lbound--;
-    if(potential[lbound]>a[i] && lbound==0){potential[lbound]=min(potential[lbound],a[i]);continue;}
-    if(potential[lbound]<=a[i])
-    {
-      int lis_length=lbound+1;
-      potential[lis_length]=min(potential[lis_length],a[i]);
-      upd=max(upd,lis_length);
+     potential[lbound]=min(potential[lbound],a[i]);
+     maxi=max(maxi,lbound);
     }
   }
-  return upd+1;
+  return maxi+1;
 }
 signed main()
 {
@@ -49,11 +42,9 @@ signed main()
   {
     int n,m;
     cin>>n>>m;
-    int a[n],b[m];
+    vector<int>a(n),b(m);
     for(int i=0;i<n;i++)cin>>a[i];
     for(int i=0;i<m;i++)cin>>b[i];
-    int lis1=lis(a,n);
-    int lis2=lis(b,m);
-    cout<<lis1+lis2<<endl;
-  }
+    cout<<lnds(a,n)+lnds(b,m)<<endl;
+  } 
 }
