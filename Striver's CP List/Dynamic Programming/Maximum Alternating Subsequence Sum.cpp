44d99f8f2bf2996f7c11c59d09bbd2deebebd994
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-20 03:30:27 +0530

    Create Maximum Alternating Subsequence Sum.cpp

---
+++ Striver's CP List/Dynamic Programming/Maximum Alternating Subsequence Sum.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/maximum-alternating-subsequence-sum/
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) 
+    {
+      long long int n=nums.size();
+      long long int dp[n][2];
+      dp[0][0]=0;
+      dp[0][1]=nums[0];
+      for(int i=1;i<n;i++)
+      {
+        dp[i][0]=max(dp[i-1][1]-nums[i],dp[i-1][0]);
+        dp[i][1]=max(dp[i-1][0]+nums[i],dp[i-1][1]);
+      }
+     return max(dp[n-1][0],dp[n-1][1]);
+    }
+};
