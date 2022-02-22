245c1f729b85f3147b878dea5276b37db0ad3707
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-23 03:35:55 +0530

    Update 1644B.cpp

--- Codeforces problems/1644B.cpp
+++ Codeforces problems/1644B.cpp
@@ -16,85 +16,37 @@ using namespace std;
 #define float double
 
 
+
 signed main()
 {
   freopen
   ios
+
   int t;
   cin>>t;
   while(t--)
   {
     int n;
     cin>>n;
-    if(n==5)
+    int a[n];
+    if(n==3)
     {
-      cout<<1<<" "<<3<<" "<<2<<" "<<4<<" "<<5<<endl;
-      cout<<1<<" "<<5<<" "<<4<<" "<<2<<" "<<3<<endl;
-      cout<<1<<" "<<4<<" "<<3<<" "<<5<<" "<<2<<endl;
-      cout<<2<<" "<<3<<" "<<4<<" "<<5<<" "<<1<<endl;
-      cout<<3<<" "<<2<<" "<<4<<" "<<5<<" "<<1<<endl;
+      cout<<"3 2 1"<<endl;
+      cout<<"3 1 2"<<endl;
+      cout<<"2 3 1"<<endl;
       continue;
-
     }
-    vector<int>ans(n+1);
-    ans[1]=1;
-    ans[2]=3;
-    ans[3]=2;
-    for(int i=4;i<=n;i++)ans[i]=i;
-
-    for(int i=1;i<=n;i++)cout<<ans[i]<<" ";
-    cout<<endl;
-
-
-
-      
-    
-    int upd=3;
-    int flag=0;
-    while(1)
+    for(int i=0;i<n;i++)a[i]=n-i;
+    for(int i=0;i<n;i++)
     {
-      if(upd+1>n)break;
-      swap(ans[upd],ans[upd+1]);
-      if(flag==0)
-      {
-        swap(ans[2],ans[3]);
-        if(n>5)swap(ans[5],ans[6]);
-
-        // if(n==5)
-        // {
-        //   cout<<1<<" "<<4<<" "<<5<<" "<<2<<" "<<3<<endl;
-        //   upd++;
-        //   continue;
-        // }
-        flag=1;
-      }
-      if(n>6 && upd==4)swap(ans[6],ans[7]);
-      upd++;
-      
-      for(int i=1;i<=n;i++)cout<<ans[i]<<" ";
+      for(int j=0;j<n;j++)cout<<a[j]<<" ";
       cout<<endl;
+      rotate(a,a+n-1,a+n);
     }
-
-    for(int i=1;i<=n;i++)ans[i]=0;
-    ans[1]=2;
-    ans[n]=1;
-    for(int i=2;i<n;i++)ans[i]=i+1;
-
-      
-    for(int i=1;i<=n;i++)cout<<ans[i]<<" ";
-    cout<<endl;
-
-        
-
-    swap(ans[1],ans[2]);
-     for(int i=1;i<=n;i++)cout<<ans[i]<<" ";
-    cout<<endl;
-
-
-     // if(yes==1)cout<<n<<endl;
-
-
   }
+ 
+
+
   
 }
 
