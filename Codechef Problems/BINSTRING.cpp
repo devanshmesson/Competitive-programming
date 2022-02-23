900b5d314f53e5d712f36060d44bd7cca3190811
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-02-23 23:21:22 +0530

    Create BINSTRING.cpp

---
+++ Codechef Problems/BINSTRING.cpp
@@ -0,0 +1,52 @@
+// taskkill /IM "a.exe" /F
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+ 
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+
+
+
+signed main()
+{
+  freopen
+  ios
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    
+    int ans=0;
+    for(int i=0;i<n-1;i++)
+    {
+      ans+=(s[i]!=s[i+1]);
+    }
+    ans++;
+    cout<<ans<<endl;
+  }
+}
+
+
+    /*1.Never overkill a probl7em.
+      2.think of test cases to clarify the approach/doubts.
+      3.Don't give up!
+      4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+      5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+      6.Check datatype of variables, sometimes wrong datatype lead to WA.
+    */ 
+    
+    
