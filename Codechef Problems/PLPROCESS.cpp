1509cc14eb2ccda023080046ad4698ef1e955703
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-01 23:05:35 +0530

    Create PLPROCESS.cpp

---
+++ Codechef Problems/PLPROCESS.cpp
@@ -0,0 +1,55 @@
+// taskkill /IM "a.exe" /F
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("out.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+ 
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+
+const int mod=998244353;
+
+signed main()
+{
+  freopen
+  ios
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int a[n];
+    int tot=0;
+    for(int i=0;i<n;i++){cin>>a[i];tot+=a[i];}
+    int mini=tot;
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+      sum+=a[i];
+      mini=min(mini,max(sum,tot-sum));
+    }
+    cout<<mini<<endl;
+
+  }
+ 
+}
+
+
+    /*1.Never overkill a problem.
+      2.think of test cases to clarify the approach/doubts.
+      3.Don't give up!
+      4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+      5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+      6.Check datatype of variables, sometimes wrong datatype lead to WA.
+    */ 
+    
+    
