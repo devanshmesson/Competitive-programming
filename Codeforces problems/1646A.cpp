8d8b86c5e81a7d3afc405979b7f9d8bd1dc1bbeb
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-04 23:12:36 +0530

    Create 1646A.cpp

---
+++ Codeforces problems/1646A.cpp
@@ -0,0 +1,45 @@
+// taskkill /IM "a.exe" /F
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("out.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+ 
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+
+
+
+signed main()
+{
+  freopen
+  ios
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n,s;
+    cin>>n>>s;
+    int a=s/(n*n);
+    cout<<a<<endl;
+
+  }
+}
+
+
+    /*1.Never overkill a problem.
+      2.think of test cases to clarify the approach/doubts.
+      3.Don't give up!
+      4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+      5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+      6.Check datatype of variables, sometimes wrong datatype lead to WA.
+    */ 
+    
+    
