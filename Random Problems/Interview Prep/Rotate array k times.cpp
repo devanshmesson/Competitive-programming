9e07de711ee5b8fcfe2eac51d0a16425c40865d7
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-07 16:53:53 +0530

    Update Rotate array k times.cpp

--- Random Problems/Interview Prep/Rotate array k times.cpp
+++ Random Problems/Interview Prep/Rotate array k times.cpp
@@ -1,8 +1,36 @@
 /*
 link - https://leetcode.com/problems/rotate-array/
 */
+
 /*
 Approach - 1
+Shift the array by 1, and do this operation k%n times
+
+Time complexity - O(N^2)
+Space complexity - O(1)
+*/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        int n=nums.size();
+        int skip = k%n;
+       
+        for(int i=0;i<skip;i++)
+        {
+          int last=nums[n-1];
+          for(int j=n-2;j>=0;j--)
+          {
+              nums[j+1]=nums[j];
+          }
+          nums[0]=last;
+        }
+        
+        
+    }
+};
+/*
+Approach - 2
 Take another vector 'v' and store ith element of nums array at [(i+k)%n]th index of v;
 Time complexity - O(N)
 Space complexity - O(N)
