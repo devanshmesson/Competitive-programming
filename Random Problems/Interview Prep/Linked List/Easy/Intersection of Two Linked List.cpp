22855b43e78e3336ccd57f505b534b550501d440
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-11 15:07:01 +0530

    Update Intersection of Two Linked List.cpp

--- Random Problems/Interview Prep/Linked List/Easy/Intersection of Two Linked List.cpp
+++ Random Problems/Interview Prep/Linked List/Easy/Intersection of Two Linked List.cpp
@@ -76,3 +76,31 @@ public:
         return NULL;
     }
 };
+
+//Approach 3 (same TC and SC as approach 2, but shorter code)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
+        if(headA==NULL || headB==NULL)return NULL;
+        ListNode *a=headA;
+        ListNode *b=headB;
+        while(a!=b)
+        {
+          if(a==NULL)a=headB;
+          else a=a->next;
+          
+          if(b==NULL)b=headA;
+          else b=b->next;
+        }
+        return a;
+    }
+};
