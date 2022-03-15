7ef0a8cb8dfb4972f1849027b6e358bf30cf08cd
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-15 20:59:36 +0530

    Update Merge k Sorted Lists (23).cpp

--- Random Problems/Interview Prep/Linked List/Hard/Merge k Sorted Lists (23).cpp
+++ Random Problems/Interview Prep/Linked List/Hard/Merge k Sorted Lists (23).cpp
@@ -4,26 +4,41 @@ Approch 1
 TC - O(N*K)
 SC - O(1)
 */
-ListNode* merge2list(ListNode *head1, *head2)
+class Solution 
 {
-  ListNode* tail=new ListNode();
-  ListNode* head=tail;
-  while(head1!=NULL && head2!=NULL)
-  {
-    if(head1->val < head2->val)
+public:
+    ListNode* merge2list(ListNode *head1, ListNode *head2)
     {
-      tail->next=head1;
-      head1=head1->next;
+      ListNode* tail=new ListNode();
+      ListNode* head=tail;
+      while(head1!=NULL && head2!=NULL)
+      {
+        if(head1->val < head2->val)
+        {
+          tail->next=head1;
+          head1=head1->next;
+        }
+        else
+        {
+          tail->next=head2;
+          head2=head2->next;
+        }
+        tail=tail->next;
+      }
+      if(head1!=NULL){tail->next=head1;}
+      if(head2!=NULL){tail->next=head2;}
+      return head->next;
     }
-    else
+    ListNode* mergeKLists(vector<ListNode*>& lists) 
     {
-      tail->next=head2;
-      head2=head2->next;
+      int k=lists.size();
+      if(k==0)return NULL;
+      if(k==1)return lists[0];
+      ListNode *sorted=lists[0];
+      for(int i=1;i<k;i++)
+      {
+        sorted=merge2list(sorted,lists[i]);
+      }
+      return sorted;
     }
-    tail=tail->next;
-  }
-  while(head1!=NULL){tail->next=head1; tail=tail->next; head1=head1->next;}
-  while(head2!=NULL){tail->next=head2; tail=tail->next; head2=head2->next;}
-  return head->next;
-}
-
+};
