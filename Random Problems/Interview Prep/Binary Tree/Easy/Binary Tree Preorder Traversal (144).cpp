42f4df363cbcc42d6377cd375e342ab121931cda
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-21 18:34:33 +0530

    Update Binary Tree Preorder Traversal (144).cpp

--- Random Problems/Interview Prep/Binary Tree/Easy/Binary Tree Preorder Traversal (144).cpp
+++ Random Problems/Interview Prep/Binary Tree/Easy/Binary Tree Preorder Traversal (144).cpp
@@ -1,4 +1,6 @@
 //https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+//Recursive
 class Solution 
 {
 public:
@@ -9,6 +11,46 @@ public:
         ans.push_back(root->val);
         preorderTraversal(root->left);
         preorderTraversal(root->right);
+        return ans;
+    }
+};
+
+//Iterative
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/*
+Think how virtual stack of recusrion works, and you just have to do it in same way in iteration.
+When too element is popped off, push right element in stack and then push the left element, 
+why? because, after popping off the left element, it should backtrack and print the right child.
+*/
+class Solution 
+{
+public:
+    
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        vector<int>ans;
+        if(root==NULL)return ans;
+        stack<TreeNode*>s;
+        s.push(root);
+        while(!s.empty())
+        {
+            TreeNode *node=s.top();
+            ans.push_back(node->val);
+            s.pop();
+            if(node->right)s.push(node->right);
+            if(node->left)s.push(node->left);
+        }
         return ans;
     }
 };
