3781df5e059a74eb31f0f4a7836536b258743e79
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-23 23:11:59 +0530

    Create XORAND.cpp

---
+++ Codechef Problems/XORAND.cpp
@@ -0,0 +1,54 @@
+// taskkill /IM "a.exe" /F
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("out.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+ 
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+signed main()
+{
+  freopen
+  ios
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int a[n+1];
+    for(int i=0;i<n;i++)cin>>a[i];
+    map<int,int>mp;
+    for(int i=0;i<n;i++)
+    {
+      for(int j=31;j>=0;j--)
+      {
+        int bit=(1<<j);
+        int res=bit&a[i];
+        if(res>0){mp[j]++;break;}
+      }
+    }
+    int ans=0;
+    for(auto x: mp)
+    {
+      int num=x.second;
+      ans+=(num)*(num-1)/2;
+    }
+    cout<<ans<<endl;
+  }
+}
+/*1.Never overkill a problem.
+  2.think of test cases to clarify the approach/doubts.
+  3.Don't give up!
+  4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+  5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+  6.Check datatype of variables, sometimes wrong datatype lead to WA.
+*/ 
