fbd27a665ddcd8f09eaddcb33920545d3ff2a99b
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-23 23:11:08 +0530

    Create UNCHANGEDOR.cpp

---
+++ Codechef Problems/UNCHANGEDOR.cpp
@@ -0,0 +1,45 @@
+// taskkill /IM "a.exe" /F
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin); freopen("out.txt","w",stdout);
+#else 
+#define freopen 
+#endif
+ 
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long int
+#define float double
+signed main()
+{
+  freopen
+  ios
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int c=log2(n);
+    int ans=0;
+    for(int i=0;i<c;i++)
+    {
+      ans+=(1<<i)-1;
+    }
+    int last=(1<<c);
+    ans+=n-last;
+    cout<<ans<<endl;
+
+  }
+}
+/*1.Never overkill a problem.
+  2.think of test cases to clarify the approach/doubts.
+  3.Don't give up!
+  4.Always pass array/string by reference in recursive function.Otherwise, it will be TLE!
+  5.don’t use multiplication operator unless necessary, Otherwise, it will be TLE!
+  6.Check datatype of variables, sometimes wrong datatype lead to WA.
+*/ 
