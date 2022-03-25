4d0ee1e35c4583cd740791898dd78b59659d9429
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-26 04:00:28 +0530

    Update Vertical Order Traversal of a Binary Tree (987).cpp

--- Random Problems/Interview Prep/Binary Tree/Hard/Vertical Order Traversal of a Binary Tree (987).cpp
+++ Random Problems/Interview Prep/Binary Tree/Hard/Vertical Order Traversal of a Binary Tree (987).cpp
@@ -1,8 +1,13 @@
 //https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
 
 /*
-Save the column numbers of the nodes, Push the node
-(complete later)
+Save the column numbers of the nodes, Push the node of ith column number to ith vector of "column" vector.
+After the traversal of a particular row, if there were multiple elements being pushed in the same column, Then
+sort the recently added elements.To do this, take anther map named "pos", which will store the starting position of
+the recently added elements for all columns.so, sort from (pos[column number])th index of "column" vector till the end of "column" vector.
+
+TC - O(N*LOG(N)) , because loop 2(highlighed) will take NLogN time after completion of level order traversal
+SC - O(N)
 */
 class Solution 
 {
@@ -28,7 +33,7 @@ public:
               if(node->left)q.push({node->left,col-1});
               if(node->right)q.push({node->right,col+1});
           }
-          for(auto x: column)
+          for(auto x: column) //Loop 2
           {
               sort(column[x.first].begin()+pos[x.first], column[x.first].end());
               pos[x.first]=x.second.size();
