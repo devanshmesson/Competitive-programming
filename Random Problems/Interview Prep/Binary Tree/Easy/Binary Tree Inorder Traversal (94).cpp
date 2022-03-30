ec4146b80c5d0bbb2c246aa7803236b89a43e60c
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-31 04:56:31 +0530

    Update Binary Tree Inorder Traversal (94).cpp

--- Random Problems/Interview Prep/Binary Tree/Easy/Binary Tree Inorder Traversal (94).cpp
+++ Random Problems/Interview Prep/Binary Tree/Easy/Binary Tree Inorder Traversal (94).cpp
@@ -1,6 +1,10 @@
 //https://leetcode.com/problems/binary-tree-inorder-traversal/
 
-//Iterative
+/*
+Recursive
+TC - O(N)
+SC - O(N)
+*/
 class Solution 
 {
 public:
@@ -16,9 +20,12 @@ public:
 };
 
 /*
-Recursive
+Iterative
 If top is not null, Keep On adding the top's left node to stack, else, remove the top(null), print the value of the top, pop off the top,
 then push top's right element. Repeat.
+
+TC -O(N)
+SC - O(N)
 */
 class Solution 
 {
@@ -48,5 +55,54 @@ public:
         }
       
         return ans;
+    }
+};
+
+/*
+Morris Traversal (Iterative)
+Explanation written in notes.
+TC - O(N)
+SC-O(1)
+*/
+class Solution 
+{
+public:
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+      vector<int>inorder;
+      while(root)
+      {
+          if(root->left==NULL)
+          {
+             inorder.push_back(root->val);
+             root=root->right;
+          }
+          else
+          {
+             TreeNode *traverse=root;
+             //Connecting the rightmost node in the left subtree of root to the root.
+             traverse=traverse->left;
+             //Second condition is necessary for the case when node is backtracked  from its rightmost node.
+             while(traverse->right && traverse->right!=root)
+             {
+               traverse=traverse->right;
+             }
+              
+             if(traverse->right==NULL)
+             {
+                traverse->right=root;
+                root=root->left;
+             }
+             else
+             {
+                 //backtracking from rightmost node in the left subtree of root to the root, breaking the link too, because its no more needed.
+                 traverse->right=NULL;
+                 inorder.push_back(root->val);
+                 root=root->right;
+             }
+              
+          }
+      }
+        return inorder;
     }
 };
