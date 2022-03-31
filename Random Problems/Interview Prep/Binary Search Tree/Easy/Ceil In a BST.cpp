1f234f1b496f6cf36f473f0b3a1b81ab3eff4a9c
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-04-01 04:39:29 +0530

    Update Ceil In a BST.cpp

--- Random Problems/Interview Prep/Binary Search Tree/Easy/Ceil In a BST.cpp
+++ Random Problems/Interview Prep/Binary Search Tree/Easy/Ceil In a BST.cpp
@@ -2,8 +2,8 @@
 
 /*
 If node's value is equal to x, then return x.
-If node's value is greater than x, then save it in a variable, and keep on finding the minimum greater element every time this type of
-condition occurs.go to node's left
+If node's value is greater than x, update the ceil and go to it's left.As we will go on the left side of node, we dont need to minimize ceil explicitly,
+It will already have a lesser number, if this conditions appears again in future.
 If node's value is lesser than x,go to node's right.
 
 TC - O(LogN), Travelling the height of tree which is at max Log N in almost all cases. 
@@ -12,12 +12,12 @@ SC - O(1)
 
 int findCeil(BinaryTreeNode<int> *node, int x)
 {
-    int minimum=2e9;
+    int minimum=-1;
     while(node)
     {
         if(node->data==x)return x;
-        if(x<(node->data)){minimum=min(minimum,node->data); node=node->left;}
+        if(x<(node->data)){minimum=node->data; node=node->left;}
         else node=node->right;
     }
-    return (minimum==2e9)?-1:minimum;  
+    return minimum;  
 }
