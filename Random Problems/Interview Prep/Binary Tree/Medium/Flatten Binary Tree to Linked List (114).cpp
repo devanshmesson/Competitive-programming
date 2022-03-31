60424164b3685b0cc74289e024465a510cd65bd4
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-04-01 00:52:21 +0530

    Update Flatten Binary Tree to Linked List (114).cpp

--- Random Problems/Interview Prep/Binary Tree/Medium/Flatten Binary Tree to Linked List (114).cpp
+++ Random Problems/Interview Prep/Binary Tree/Medium/Flatten Binary Tree to Linked List (114).cpp
@@ -12,7 +12,7 @@ to the flattened preorder traversal for left subtree.
 
 To do this, We traverse in reverse preorder fashion which Right, left, root. First right subtree of root is made ready to get connected to
 left subtree. A prev pointer is used to store the first node of the preorder traversal of right subtree.As soon as right is ready, we go to left.
-The last node(leftmost) of the left subtree's right pointer gets connected to prev and left pointer gets pointed to NULL, prev gets 
+The last node (rightmost) of the left subtree's right pointer gets connected to prev and left pointer gets pointed to NULL, prev gets 
 updated to this node.
 
 To understand it more clearly, start with a root, having only one node on left and one node on right.
@@ -40,7 +40,7 @@ public:
 
 /*
 Iterative:
-Observation : last node(leftmost) of the left subtree of any node gets connected to the first node of the right subtree.
+Observation : last node(rightmost) of the left subtree of any node gets connected to the first node of the right subtree.
 
 We will use stack.So, first node of right subtree should be before the last node of left subtree.Stack contains root initially.
 Start from root. Push the right child , then push the left child. Now connect the root's right to the left child and 
@@ -70,6 +70,42 @@ public:
         if(root->left)s.push(root->left);
         if(!s.empty())root->right=s.top();
         root->left=NULL;
+      }
+    }
+};
+/*
+Perform Morris Traversal.
+Observation : last node(rightmost) of the left subtree of any node gets connected to the first node of the right subtree.
+For a node, go to its rightmost node in its left subtree. Rightmost node in code means, go left and then go extreme right. If extreme right node contains
+left, then dont go there. Just connect the extreme right to the node's right.After that, Connect the root's right ptr to root' left node. and connect root's
+left pointer to NULL.and for further traversal, go to root's right. So basically, as we are traversing, er are flattening it.
+
+Reason for not going to left after extreme right is reached, is because
+When extreme right becomes the "node"(basically, root), then its subtree's extreme right will be connected to the node' right which is eventually,
+the right node of root of whole tree.So, the right gets updated till the last node. And what about the root which was also connected to the 
+right node of root of whole tree? This right ptr is now pointing to it's left node. So this how it works.
+
+TC - O(N)
+SC - O(1)
+*/
+
+class Solution 
+{
+public:
+    void flatten(TreeNode * root) 
+    {
+      while(root)
+      {
+          if(root->left)
+          {
+              TreeNode *traverse=root;
+              traverse=traverse->left;
+              while(traverse->right)traverse=traverse->right;
+              traverse->right=root->right;
+              root->right=root->left;
+              root->left=NULL;
+          }
+          root=root->right;
       }
     }
 };
