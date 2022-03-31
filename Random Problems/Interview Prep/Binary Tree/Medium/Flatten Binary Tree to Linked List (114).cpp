9e1a794c2a751895b2a555e3477494563d16d354
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-03-31 19:44:06 +0530

    Update Flatten Binary Tree to Linked List (114).cpp

--- Random Problems/Interview Prep/Binary Tree/Medium/Flatten Binary Tree to Linked List (114).cpp
+++ Random Problems/Interview Prep/Binary Tree/Medium/Flatten Binary Tree to Linked List (114).cpp
@@ -12,7 +12,7 @@ to the flattened preorder traversal for left subtree.
 
 To do this, We traverse in reverse preorder fashion which Right, left, root. First right subtree of root is made ready to get connected to
 left subtree. A prev pointer is used to store the first node of the preorder traversal of right subtree.As soon as right is ready, we go to left.
-The last node(rightmost) of the left subtree's right pointer gets connected to prev and left pointer gets pointed to NULL, prev gets 
+The last node(leftmost) of the left subtree's right pointer gets connected to prev and left pointer gets pointed to NULL, prev gets 
 updated to this node.
 
 To understand it more clearly, start with a root, having only one node on left and one node on right.
@@ -34,6 +34,42 @@ public:
         root->right=prev;
         root->left=NULL;
         prev=root;
+      }
+    }
+};
+
+/*
+Iterative:
+Observation : last node(leftmost) of the left subtree of any node gets connected to the first node of the right subtree.
+
+We will use stack.So, first node of right subtree should be before the last node of left subtree.Stack contains root initially.
+Start from root. Push the right child , then push the left child. Now connect the root's right to the left child and 
+root's left to NULL.Then in next iteration, pick the top element. which will the left child of it's root.
+If it's left child & right child  is NULL, then this is the left most node of it's root.So, the next top element
+is the right child of the root that we are talking about. So now this leftmost node gets connected to the top element 
+which is the first right element.
+
+TC - O(N)
+SC - O(N)
+
+
+*/
+class Solution 
+{
+public:
+    void flatten(TreeNode * root) 
+    {
+      
+      stack<TreeNode*>s;
+      if(root)s.push(root);
+      while(!s.empty())
+      {
+        TreeNode *root=s.top();
+        s.pop();
+        if(root->right)s.push(root->right);
+        if(root->left)s.push(root->left);
+        if(!s.empty())root->right=s.top();
+        root->left=NULL;
       }
     }
 };
