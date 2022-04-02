72679ff37a76b62995fa569719825bcccf13cdc1
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-04-02 19:49:36 +0530

    Update Inorder Successor in BST.cpp

--- Random Problems/Interview Prep/Binary Search Tree/Medium/Inorder Successor in BST.cpp
+++ Random Problems/Interview Prep/Binary Search Tree/Medium/Inorder Successor in BST.cpp
@@ -10,6 +10,44 @@ We can also do a binary search and find next greater element than value of node 
 TC- O(N)
 SC -O(N)
 */
+class Solution{
+  public:
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+    
+    Node * inOrderSuccessor(Node *root, Node *x)
+    {
+       Node *suc=NULL;
+       vector<pair<int,Node*>>inorder;
+       while(root)
+       {
+         if(root->left==NULL){inorder.push_back({root->data,root});root=root->right;} 
+         else 
+         {
+           //Finding the rightmost node in the left subtree of the root and connecting it to root.
+           Node *traverse=root;
+           //going to left
+           traverse=traverse->left;
+           //going to extreme right
+           while(traverse->right && traverse->right!=root)traverse=traverse->right;
+           if(!traverse->right){traverse->right=root; root=root->left;}
+           else {inorder.push_back({root->data,root}); traverse->right=NULL; root=root->right;}
+          }  
+       }
+      //Binary search
+       int l=0,r=inorder.size()-1,mid;
+       int ans=-1;
+       while(l<=r)
+       {
+           mid=(l+r)/2;
+           if(inorder[mid].first<=x->data)l=mid+1;
+           else {r=mid-1; ans=mid;}
+       }
+       if(ans==-1)return NULL;
+       return inorder[ans].second;
+    }
+    
+};
+
 
 
 /*
