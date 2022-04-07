a49662d55f871bc35c4feadd7e54c96af8fc8594
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-04-07 06:28:50 +0530

    Update Topological Sort.cpp

--- Random Problems/Interview Prep/Graphs/Medium/Topological Sort.cpp
+++ Random Problems/Interview Prep/Graphs/Medium/Topological Sort.cpp
@@ -52,3 +52,58 @@ class Solution
 	  return toposort;
 	}
 };
+
+/*
+Performed BFS(Kahn's algorithm).
+Observation 1 - if there a node u, who is an adjacent node of nodes x,y,z.In other words,
+x,y,z are pre-requisites for node u.If these x y z are already included in the topological sort
+then, that means pre-requites for node u are done, so node u can now be included in the topo sort.
+
+Observation 2 - First node of Topological sort has no pre-requisite.In other words, it has 0 indegree.
+
+An indegree of a node defines it's number of pre-requites.So we will calculate indegree of each node 
+in the graph and the nodes with 0 indegree will be present at the beginning of the topo sort.
+
+Nodes with 0 indegree will start the BFS, so they all are pushed into the queue.Indegree of
+adjacent nodes of current node in the bfs will be reduced by 1.Reducing by 1 is signifying 
+that a pre-requisite of adjacent node is present in the topo sort.If Indegree of
+adjacent nodes of current node becomes 0, that means, all pre-requisites for this node
+are present in the topo sort.So, now this adjacent node can enter into toposort.So, push this adjacent
+node into the queue.
+
+So, we are adding those nodes in the toposort whose pre-requites are completed.
+
+TC - O(N+E)
+SC - O(N)
+*/
+class Solution
+{
+	public:
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	   queue<int>tp;
+	   int indegree[V]={0};
+	   for(int i=0;i<V;i++)
+	   {
+	     for(int j=0;j<adj[i].size();j++)indegree[adj[i][j]]++;
+	   }
+	   for(int i=0;i<V;i++)if(indegree[i]==0)tp.push(i);
+	   vector<int>toposort;
+       while(!tp.empty())
+       {
+          int node=tp.front();
+          toposort.push_back(node);
+          tp.pop();
+          for(int i=0;i<adj[node].size();i++)
+          {
+              int adjacent=adj[node][i];
+              indegree[adjacent]--;
+              if(indegree[adjacent]==0)
+              {
+                  tp.push(adjacent);
+              }
+          }
+       }
+       return toposort;
+	}
+};
