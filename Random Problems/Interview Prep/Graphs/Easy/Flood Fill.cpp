7d9098634fcb4986b42a6f71970128aba6c8c6e4
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-04-21 01:13:36 +0530

    Update Flood Fill.cpp

--- Random Problems/Interview Prep/Graphs/Easy/Flood Fill.cpp
+++ Random Problems/Interview Prep/Graphs/Easy/Flood Fill.cpp
@@ -12,11 +12,10 @@ public:
         queue<pair<int,int>>q;
         int rows=image.size();
         int cols=image[0].size();
-        vector<vector<int>>filled(rows,vector<int>(cols));
         q.push({sr,sc});
         vector<pair<int,int>>direction={{-1,0},{1,0},{0,-1},{0,1}};
-        filled[sr][sc]=1;
         int startcolor=image[sr][sc];
+        image[sr][sc]=newColor;
         while(!q.empty())
         {
           int x=q.front().first;
@@ -26,19 +25,12 @@ public:
           {
              int newx=x+direction[i].first;
              int newy=y+direction[i].second;
-             if(newx>=0 && newy>=0 && newx<rows && newy<cols && filled[newx][newy]==0 && image[newx][newy]==startcolor)
+             if(newx>=0 && newy>=0 && newx<rows && newy<cols  && image[newx][newy]==startcolor && image[newx][newy]!=newColor)
              {
-               filled[newx][newy]=1;
+               image[newx][newy]=newColor;
                q.push({newx,newy});
              }
           }
-        }
-        for(int i=0;i<rows;i++)
-        {
-         for(int j=0;j<cols;j++)
-         {
-           if(filled[i][j]==1)image[i][j]=newColor;
-         }
         }
         return image;
     }
