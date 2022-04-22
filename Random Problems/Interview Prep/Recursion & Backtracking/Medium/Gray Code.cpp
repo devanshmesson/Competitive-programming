6ce5926c71546c470dda877c0bcdece12d365cf0
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-04-23 00:20:06 +0530

    Update Gray Code.cpp

--- Random Problems/Interview Prep/Recursion & Backtracking/Medium/Gray Code.cpp
+++ Random Problems/Interview Prep/Recursion & Backtracking/Medium/Gray Code.cpp
@@ -5,13 +5,19 @@ For N=1, answer is [00,01]
 For N=2, answer can be built as:
 Add a unset bit at the starting of the all the numbers belonging to answer of (N-1) as follows:
 000
-001
+001 (a)
 
 Add a set bit at the starting of the all the numbers belonging to answer of (N-1) as follows:
 100
-101
+101 (b)
 
 Merge these two answers, but there is problem if we merge them in a straight way:
+The probelm is, that a and b have a difference of two bits.
+To resolve this, reverse the order of the number which started with set bit.
+
+TC- O(2^N) 2^0 + 2^1 + 2^2 +---+ 2^(n-1)=2^n
+SC - O(N)
+
 
 
 */
