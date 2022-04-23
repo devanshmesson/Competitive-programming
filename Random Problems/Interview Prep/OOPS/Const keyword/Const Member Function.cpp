7b9dfdaaea7fb0f25d0df2e60deab309b9929960
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-04-24 00:51:17 +0530

    Create Const Member Function.cpp

---
+++ Random Problems/Interview Prep/OOPS/Const keyword/Const Member Function.cpp
@@ -0,0 +1,68 @@
+class employee
+{
+  public:
+  string company;
+  int age;
+  string password;
+  char *name;
+  
+  employee()
+  {
+    cout<<"Default constructor called"<<endl;
+  }
+  //Parametrized Constructor 
+  employee(string company)
+  {
+    this->company=company;
+    this->age=age;
+  }
+  //Parametrized Constructor 
+ employee(string company, int age, char *name) 
+  {
+    this->company=company;
+    this->age=age;
+    this->name=new char[strlen(name)+1];
+    strcpy(this->name,name);
+  }
+  //User defined copy constructor
+  employee(employee &old_emp)
+  {
+    
+    company=old_emp.company;
+    age=old_emp.age;
+    //Newly allocated memory, avoids shallow copying by doing deep copy.
+    this->name=new char[strlen(old_emp.name)+1];
+    strcpy(name,old_emp.name);
+  }
+ 
+  //setter (const function)
+  //This produces error because The idea of const functions is not to allow them to modify the object on which they are called
+  void setcompany(string comp,string password) const 
+  {
+    if(password=="abcd")company=comp;
+  }
+  //getter
+  string getcompany() 
+  {
+    return company;
+  }
+  void print()
+  {
+    cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"Company:"<<company<<endl;
+    cout<<endl;
+  }
+  ~employee()
+  {
+    cout<<"Destructor called"<<endl;
+  }
+
+};
+
+signed main()
+{
+  char name1[]="devansh";
+  employee devansh("Amazon",22,name1);
+  devansh.setcompany("Google","abcd");
+}
