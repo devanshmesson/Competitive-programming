08fa6981706979f656f29459a9099f75f5064d5f
Devansh Messon <46960431+devanshmesson@users.noreply.github.com>
2022-04-24 23:18:40 +0530

    Create Private Mode.cpp

---
+++ Random Problems/Interview Prep/OOPS/Pillars of OOPS/Inheritance/Access Modes/Private Mode.cpp
@@ -0,0 +1,89 @@
+
+class Human
+{
+  protected:
+  int weight;
+
+  public:
+  int age;
+  string name;
+
+  private:
+  int income;
+  
+  public:
+  int getAge()
+  {
+    return this->age;
+  }
+  void setAge(int age)
+  {
+    this->age=age;
+  }
+};
+
+class Male: private Human
+{
+ public:
+ int skin_color;
+ void sleep()
+ {
+  cout<<"Male is sleeping"<<endl;
+ }
+ int getWeight()
+ {
+  return this->weight;
+ }
+ /*
+ int getIncome()
+ {
+  return this->income; //this will produce error,because income is not available in derived class.
+ }
+ */
+
+ int getAge()
+ {
+  return this->age;
+ }
+ int setAge(int age)
+ {
+  this->age=age;
+ }
+
+};
+
+
+signed main()
+{
+ 
+  Male m;
+  /*
+  Access mode: private
+  public members of base class have private access mode
+  in derived class.
+  */
+  //cout<<m.age<<endl; //This will produce error
+  m.setAge(22); //Modifying private members through setter
+  cout<<m.getAge()<<endl; //Accessing private members through getter
+
+  //---------------------------------------------------------
+
+  /*
+  Access mode: private
+  protected members of base class have private access mode
+  in derived class.
+  */
+  //cout<<m.weight<<endl;//This will produce error
+  cout<<m.getWeight()<<endl; //Accessing private members through getter
+
+  //---------------------------------------------------------
+
+  /*
+  Access mode: private
+  private members of base class have no access in derived class.
+  They are not present in derived class.
+  */
+
+  // cout<<m.income<<endl; // this will produce error
+  //cout<<m.getIncome()<<endl; //getIncome() will produce error
+}
